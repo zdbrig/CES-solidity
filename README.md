9a924aadf350d7c5b958fdb8861814e881013dd6
bacem <bacem.bergaoui@gmail.com>
2022-04-07 21:13:06 +0100

    add config for kovan

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CES-solidity
 CrossChain transaction between Ethereum and Solana
+
+#How to compile
+
+create file .secret under main directory that contains ethrereum 12 words mnemonic
+
+create file .infura under main directory that contains infura id
+
+run `npm install` 
+
+
--- truffle-config.js
+++ truffle-config.js
@@ -18,11 +18,12 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
+ const HDWalletProvider = require('@truffle/hdwallet-provider');
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
-
+ const fs = require('fs');
+ const mnemonic = fs.readFileSync(".secret").toString().trim();
+ const infura_key = fs.readFileSync(".infura").toString().trim();
+ const infura_kovan_url = "https://kovan.infura.io/v3/" + infura_key;
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -41,11 +42,11 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+    },
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -57,31 +58,31 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
-    // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-    // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+   kovan: {
+    provider: () => new HDWalletProvider(mnemonic, infura_kovan_url ),
+    network_id: 42,       // Ropsten's id
+    gas: 5500000,        // Ropsten has a lower block limit than mainnet
+    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
   },
+  // Useful for private networks
+  // private: {
+  // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+  // network_id: 2111,   // This network is yours, in the cloud.
+  // production: true    // Treats this network as if it was a public net. (default: false)
+  // }
+},
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
-    // timeout: 100000
-  },
+  // timeout: 100000
+},
 
-  // Configure your compilers
-  compilers: {
-    solc: {
-      version: "0.8.13",      // Fetch exact version from solc-bin (default: truffle's version)
+// Configure your compilers
+compilers: {
+  solc: {
+    version: "0.8.13",      // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
@@ -91,7 +92,7 @@ module.exports = {
       //  evmVersion: "byzantium"
       // }
     }
-  },
+},
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
   // false to enabled: true. The default storage location can also be
